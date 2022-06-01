573dfb1b0a2b3bd3960c4b3b0f29b6fa973d7f89
SatrioAjiTI <78513122+SatrioAjiTI@users.noreply.github.com>
2022-06-01 18:06:49 +0700

    Add files via upload

---
+++ UG13-71200538/src/main/java/com/ug13/rumahsakit/Suster.java
@@ -0,0 +1,18 @@
+package com.ug13.rumahsakit;
+
+public class Suster {
+    private String nama;
+
+    public Suster(String nama) {
+        this.nama = nama;
+    }
+
+    public void screening(Pasien pasien, Jadwal jadwal){
+        if(pasien.getPenyakit() == null){
+            System.out.println("Silahkan melakukan regis");
+        }
+        else {
+            jadwal.setStatusScreening(true);
+        }
+    }
+}
